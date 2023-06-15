c4a9ae296c6b80a525afe29dfe1e17d000dac848
abdulshukoor <ashukoor12@gmail.com>
2023-06-15 07:45:06 -0400

    added Votes schemla, route and updated post routes, added alembic

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel,EmailStr
 from datetime import datetime
 from typing import Optional
+from pydantic.types import conint
 
 
 class PostBase(BaseModel):
@@ -30,6 +31,12 @@ class Post(PostBase):
 
   class Config:
      orm_mode = True
+class PostOut(BaseModel):
+  Post: Post
+  votes: int
+
+  class Config:
+   orm_mode = True
 
 class UserCreate(BaseModel):
     email: EmailStr
@@ -46,4 +53,11 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[str]
+
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1)
+    # allow 1,0
+    #it also allow negative value
     
